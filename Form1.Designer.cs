dd64274228204ef785fc2f0cabd5242f95222564
george.castilho <george.castilho@apexbrasil.com.br>
2023-11-10 21:22:15 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace SplashScreen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelProgress = new System.Windows.Forms.Panel();
+            this.mySqlConnection = new MySql.Data.MySqlClient.MySqlConnection();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Location = new System.Drawing.Point(-1, 294);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(522, 14);
+            this.panel1.TabIndex = 1;
+            // 
+            // panelProgress
+            // 
+            this.panelProgress.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panelProgress.Location = new System.Drawing.Point(-9, 293);
+            this.panelProgress.Name = "panelProgress";
+            this.panelProgress.Size = new System.Drawing.Size(10, 16);
+            this.panelProgress.TabIndex = 2;
+            // 
+            // mySqlConnection
+            // 
+            this.mySqlConnection.ConnectionString = "server=127.0.0.1;user id=dev;password=dev@master;database=braexp_local";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(518, 310);
+            this.Controls.Add(this.panelProgress);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panelProgress;
+        private MySql.Data.MySqlClient.MySqlConnection mySqlConnection;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SplashScreen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                mySqlConnection.Open();
+                if (mySqlConnection.State == ConnectionState.Open)
+                {
+                    var stm = "SELECT * FROM wp_users";
+                    var cmd = new MySqlCommand(stm, mySqlConnection);
+
+                    var resultado = cmd.ExecuteScalar().ToString();
+                    Console.WriteLine($"{resultado}");
+                }
+            }
+            catch (System.Exception e)
+            {
+                MessageBox.Show(e.Message.ToString());
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            panelProgress.Width += 11;
+            if (panelProgress.Width > 522)
+            {
+                timer1.Stop();
+                Form2 form2 = new Form2();
+                form2.Show();
+                this.Hide();
+            }
+        }
+
+       
+    }
+}
